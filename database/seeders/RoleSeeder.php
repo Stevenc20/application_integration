6cf3d66ee44a71821fb940f42ae1f160847332dc
Stevenc20 <stevenchristian2005@gmail.com>
2026-08-10 09:58:50 +0700

    feat(QA): Add QA specific roles to RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -9,7 +9,7 @@ class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        $roles = ['SuperAdmin', 'Admin', 'Director', 'User'];
+        $roles = ['SuperAdmin', 'Admin', 'Director', 'User', 'Group Leader', 'Foreman', 'Supervisor', 'Leader', 'Operator'];
         foreach ($roles as $name) {
             Role::firstOrCreate(['role_name' => $name]);
         }
