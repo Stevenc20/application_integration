e91ffdb6597942ceb7ad19fa02fa2618a90d43b8
Stevenc20 <stevenchristian2005@gmail.com>
2026-08-24 09:52:39 +0700

    remove another file

--- test_api.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-\ = require_once __DIR__.'/bootstrap/app.php';
-\ = \->make(Illuminate\\Contracts\\Console\\Kernel::class);
-\->bootstrap();
-\ = app(\\App\\Services\\DashboardRealtimeService::class)->getLineMetrics('A', date('Y-m-d'), 1);
-echo json_encode(\, JSON_PRETTY_PRINT);
