d337c49c45f1725bbb788d71c74d1573628a5e06
Stevenc20 <stevenchristian2005@gmail.com>
2026-09-05 17:19:22 +0700

    Remove local folders and test files from repository

--- digitalisasi/stamping/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}), required=True)
